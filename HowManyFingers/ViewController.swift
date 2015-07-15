4da86b7d5bfb5b1ff6984a9339dd52797722584f
Mohit Jain <mohit.jain@move.com>
2015-07-15 14:40:40 -0700

    Demo project - how many fingers

--- HowManyFingers/ViewController.swift
+++ HowManyFingers/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var guessTextField: UITextField!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBAction func guessAction(sender: AnyObject) {
+       
+        let computedAnswer = arc4random_uniform(6)
+        
+        let guessedAnswer = Int(guessTextField.text!)
+        
+        if (guessedAnswer != nil) {
+        
+            if guessedAnswer == Int(computedAnswer) {
+                self.answerLabel.text = "Your guess is correct"
+            }
+            else {
+                self.answerLabel.text = "Your guess is incorrect"
+            }
+        }
+        else {
+            self.answerLabel.text = "Please enter number between 1-5";
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
